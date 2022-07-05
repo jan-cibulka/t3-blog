13eddcbcc8f0aa9a1ce4e29d43022aa68f5d0300
jan.cibulka <jan.cibulka@hotmail.cz>
2022-07-05 11:58:46 +0200

    create trpc context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { withTRPC } from "@trpc/next";
+
+import { loggerLink } from "@trpc/client/links/loggerLink";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import superjson from "superjson";
+import { AppRouter } from "../server/route/app.router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+      : "http://localhost:3000/api/trpc";
+    const links = [loggerLink(), httpBatchLink({ maxBatchSize: 10, url })];
+    return {
+      links,
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            staleTime: 60,
+          },
+        },
+      },
+      headers() {
+        if (ctx?.req) {
+          return { ...ctx.req.headers, "x-ssr": "1" };
+        }
+        return {};
+      },
+      transformer: superjson,
+    };
+  },
+  ///consider wether to use this or not (fetch data on server or on client)
+  ssr: false,
+})(MyApp);
